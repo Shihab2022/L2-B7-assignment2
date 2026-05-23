fac95a032cabf5fc200cc2b0200cd24bb133a350
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2026-05-23 10:33:37 +0600

    connect db

---
+++ src/db/index.ts
@@ -0,0 +1,42 @@
+import { Pool } from "pg";
+import config from "../app/config";
+
+export const pool = new Pool({
+  connectionString: config.database_url,
+});
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor' 
+        CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+            `);
+
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS issues (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL CHECK (LENGTH(description) >= 20),
+    type VARCHAR(20) NOT NULL 
+        CHECK (type IN ('bug', 'feature_request')),
+    status VARCHAR(20) NOT NULL DEFAULT 'open' 
+        CHECK (status IN ('open', 'in_progress', 'resolved')),
+    reporter_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);  
+        `);
+
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { Server } from "http";
-
 import config from "./app/config";
 import app from "./app";
+import { initDB } from "./db";
 
 async function main() {
   const port = config.port;
+  initDB();
   const server: Server = app.listen(port, () => {
     console.log(`App server is running on  ${port} `);
   });
